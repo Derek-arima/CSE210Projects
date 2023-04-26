4720af3c481566457f4d2914fc5d02f29bd4a4da
Derek-arima <darima@byui.edu>
2023-04-26 12:34:34 -0600

    Finished Prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?");
+        string gradePercentage = Console.ReadLine();
+        int percentage = int.Parse(gradePercentage);
+
+        string letterGrade = "S";
+
+        if (percentage >= 90)
+        {
+            letterGrade = "A";
+        } 
+
+        else if (percentage >= 80 && percentage <90)
+        {
+            letterGrade = "B";
+        }
+
+        else if (percentage >= 70 && percentage <80)
+        {
+            letterGrade = "C";
+        }
+
+        else if (percentage >= 60 && percentage <70)
+        {
+            letterGrade = "D";
+        }
+
+        if (percentage < 60)
+        {
+            letterGrade = "F";
+        }
+
+        Console.Write($"Your grade is: {letterGrade}");
     }
 }
